a9b17a99115ca64ae4edf5df4c13d371f6ddb287
levin_serg <levineserg@gmail.com>
2023-01-25 08:32:27 +0400

    Решил задачи 41, 43

---
+++ task41/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write("Введите число ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+Console.Write("Введите длинну массива ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[length];
+FillArray(array);
+Console.Write("Ваш массив: ");
+Console.WriteLine(string.Join(", ", array));
+int positiveNumbers = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        positiveNumbers += 1;
+    }
+}
+Console.Write($"В этом  массиве {positiveNumbers} положительных");
---
+++ task43/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, 
+// y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("Введите  b1");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите  k1");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите  b2");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите  k2");
+double k2 = Convert.ToDouble(Console.ReadLine());
+if (k1 == k2 && b1 == b2)
+{
+    Console.WriteLine("Прямые совпадают");
+}
+else if (k1 == k2)
+{
+    Console.WriteLine("Прямые параллельны");
+}
+else
+{
+    double x = Math.Round((b2 - b1) / (k1 - k2), 2);
+    double y = Math.Round(k1 * x + b1, 2);
+    Console.WriteLine($"координаты точки пересечения {x} ; {y}");
+}
